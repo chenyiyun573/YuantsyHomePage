3fd579e821b16655bdbc5a3eea005d028ea29eac
lw0000 <8765213+lw0000@user.noreply.gitee.com>
2024-08-31 15:01:48 +0800

    update eslint config,router,Home

--- src/components/SiteHeader/index.jsx
+++ src/components/SiteHeader/index.jsx
@@ -1,84 +1,130 @@
+import { Link, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 const SiteHeader = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const handleClick = (event) => {
+      if (event.target && event.target.matches("a.internal-link")) {
+        event.preventDefault(); // 防止浏览器默认行为
+        const href = event.target.getAttribute("href");
+        navigate(href); // 使用 React Router 的 navigate
+      }
+    };
+
+    window.addEventListener("click", handleClick);
+
+    return () => {
+      window.removeEventListener("click", handleClick);
+    };
+  }, [navigate]);
   return (
     //  <!-- Site header -->
     <header className="fixed top-2 md:top-6 w-full z-30">
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
         <div className="relative flex items-center justify-between gap-3 h-14 rounded-2xl px-3 backdrop-blur-sm bg-white/90 shadow-lg shadow-black/[0.03] before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(theme(colors.gray.100),theme(colors.gray.200))_border-box] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)] before:[mask-composite:exclude_!important] before:pointer-events-none">
-
           {/* <!-- Site branding --> */}
           <div className="flex-1 flex items-center">
             {/* <!-- Logo --> */}
-            <a class="inline-flex" href="index.html" aria-label="Cruip">
+            <Link class="inline-flex" to="/home" aria-label="Cruip">
               <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28">
-                <path class="fill-blue-500" fill-rule="evenodd" d="M15.052 0c6.914.513 12.434 6.033 12.947 12.947h-5.015a7.932 7.932 0 0 1-7.932-7.932V0Zm-2.105 22.985V28C6.033 27.487.513 21.967 0 15.053h5.015a7.932 7.932 0 0 1 7.932 7.932Z" clip-rule="evenodd" />
-                <path class="fill-blue-300" fill-rule="evenodd" d="M0 12.947C.513 6.033 6.033.513 12.947 0v5.015a7.932 7.932 0 0 1-7.932 7.932H0Zm22.984 2.106h5.015C27.486 21.967 21.966 27.487 15.052 28v-5.015a7.932 7.932 0 0 1 7.932-7.932Z" clip-rule="evenodd" />
+                <path
+                  class="fill-blue-500"
+                  fill-rule="evenodd"
+                  d="M15.052 0c6.914.513 12.434 6.033 12.947 12.947h-5.015a7.932 7.932 0 0 1-7.932-7.932V0Zm-2.105 22.985V28C6.033 27.487.513 21.967 0 15.053h5.015a7.932 7.932 0 0 1 7.932 7.932Z"
+                  clip-rule="evenodd"
+                />
+                <path
+                  class="fill-blue-300"
+                  fill-rule="evenodd"
+                  d="M0 12.947C.513 6.033 6.033.513 12.947 0v5.015a7.932 7.932 0 0 1-7.932 7.932H0Zm22.984 2.106h5.015C27.486 21.967 21.966 27.487 15.052 28v-5.015a7.932 7.932 0 0 1 7.932-7.932Z"
+                  clip-rule="evenodd"
+                />
               </svg>
-            </a>
+            </Link>
           </div>
 
           {/* <!-- Desktop navigation --> */}
           <nav className="hidden md:flex md:grow">
-
             {/* <!-- Desktop menu links --> */}
             <ul className="text-sm flex grow justify-center flex-wrap items-center gap-4 lg:gap-8">
               <li className="px-3 py-1">
-                <a className="text-gray-700 hover:text-gray-900 flex items-center transition" href="pricing.html">Pricing</a>
+                <Link
+                  className="text-gray-700 hover:text-gray-900 flex items-center transition"
+                  to="/pricing"
+                >
+                  Pricing
+                </Link>
               </li>
               <li className="px-3 py-1">
-                <a className="text-gray-700 hover:text-gray-900 flex items-center transition" href="customers.html">Customers</a>
+                <Link
+                  className="text-gray-700 hover:text-gray-900 flex items-center transition"
+                  to="/customers"
+                >
+                  Customers
+                </Link>
               </li>
               <li className="px-3 py-1">
-                <a className="text-gray-700 hover:text-gray-900 flex items-center transition" href="blog.html">Blog</a>
+                <Link
+                  className="text-gray-700 hover:text-gray-900 flex items-center transition"
+                  to="/blog"
+                >
+                  Blog
+                </Link>
               </li>
               <li className="px-3 py-1">
-                <a className="text-gray-700 hover:text-gray-900 flex items-center transition" href="documentation.html">Docs</a>
+                <Link
+                  className="text-gray-700 hover:text-gray-900 flex items-center transition"
+                  to="/documentation"
+                >
+                  Docs
+                </Link>
               </li>
               {/* <!-- 1st level: hover --> */}
               <div
                 dangerouslySetInnerHTML={{
                   __html: `
-                   <li
-                                class="relative flex items-center gap-0.5 px-3 py-1"
-                                x-data="{ open: false }"
-                                @mouseenter="open = true"
-                                @mouseleave="open = false"
-                            >
-                                <span
-                                    class="text-gray-700 hover:text-gray-900 flex items-center cursor-pointer transition"
-                                    aria-haspopup="true"
-                                    :aria-expanded="open"
-                                >Extra</span>
-                                <button
-                                    class="shrink-0 p-1"
-                                    :aria-expanded="open"
-                                    @click.prevent="open = !open"
-                                >
-                                    <span class="sr-only">Show submenu for "Extra"</span>
-                                    <svg class="fill-gray-400" xmlns="http://www.w3.org/2000/svg" width="10" height="6">
-                                        <path d="m1.06.19 3.5 3.5 3.5-3.5 1.061 1.06-4.56 4.56L0 1.25 1.06.19Z" />
-                                    </svg>
-                                </button>
-                                <ul
-                                    class="origin-top-left absolute top-full left-0 w-36 bg-white p-2 rounded-xl shadow-lg shadow-black/[0.03] border border-gray-100"
-                                    x-show="open"
-                                    x-transition:enter="transition ease-out transform"
-                                    x-transition:enter-start="opacity-0 -translate-y-2"
-                                    x-transition:enter-end="opacity-100 scale-100"
-                                    x-transition:leave="transition ease-out"
-                                    x-transition:leave-start="opacity-100"
-                                    x-transition:leave-end="opacity-0"
-                                    x-cloak
-                                    @focusout="await $nextTick();!$el.contains($focus.focused()) && (open = false)"
-                                >
-                                    <li>
-                                        <a class="text-sm text-gray-700 hover:bg-gray-100 flex py-1.5 px-2 rounded-lg" href="support.html">Support center</a>
-                                    </li>
-                                    <li>
-                                        <a class="text-sm text-gray-700 hover:bg-gray-100 flex py-1.5 px-2 rounded-lg" href="apps.html">Apps</a>
-                                    </li>
-                                </ul>
-                            </li>
-                  `
+                <li
+                    class="relative flex items-center gap-0.5 px-3 py-1"
+                    x-data="{ open: false }"
+                    @mouseenter="open = true"
+                    @mouseleave="open = false"
+                >
+                    <span
+                        class="text-gray-700 hover:text-gray-900 flex items-center cursor-pointer transition"
+                        aria-haspopup="true"
+                        :aria-expanded="open"
+                    >Extra</span>
+                    <button
+                        class="shrink-0 p-1"
+                        :aria-expanded="open"
+                        @click.prevent="open = !open"
+                    >
+                        <span class="sr-only">Show submenu for "Extra"</span>
+                        <svg class="fill-gray-400" xmlns="http://www.w3.org/2000/svg" width="10" height="6">
+                            <path d="m1.06.19 3.5 3.5 3.5-3.5 1.061 1.06-4.56 4.56L0 1.25 1.06.19Z" />
+                        </svg>
+                    </button>
+                    <ul
+                        class="origin-top-left absolute top-full left-0 w-36 bg-white p-2 rounded-xl shadow-lg shadow-black/[0.03] border border-gray-100"
+                        x-show="open"
+                        x-transition:enter="transition ease-out transform"
+                        x-transition:enter-start="opacity-0 -translate-y-2"
+                        x-transition:enter-end="opacity-100 scale-100"
+                        x-transition:leave="transition ease-out"
+                        x-transition:leave-start="opacity-100"
+                        x-transition:leave-end="opacity-0"
+                        x-cloak
+                        @focusout="await $nextTick();!$el.contains($focus.focused()) && (open = false)"
+                    >
+                        <li>
+                            <a class="text-sm text-gray-700 hover:bg-gray-100 flex py-1.5 px-2 rounded-lg internal-link" href="/support">Support center</a>
+                        </li>
+                        <li>
+                            <a class="text-sm text-gray-700 hover:bg-gray-100 flex py-1.5 px-2 rounded-lg internal-link" href="/apps">Apps</a>
+                        </li>
+                    </ul>
+                </li>
+                  `,
                 }}
               ></div>
             </ul>
@@ -87,10 +133,20 @@ const SiteHeader = () => {
           {/* <!-- Desktop sign in links --> */}
           <ul className="flex-1 flex justify-end items-center gap-3">
             <li>
-              <a className="btn-sm text-gray-800 bg-white hover:bg-gray-50 shadow" href="signin.html">Login</a>
+              <Link
+                className="btn-sm text-gray-800 bg-white hover:bg-gray-50 shadow"
+                to="/signin"
+              >
+                Login
+              </Link>
             </li>
             <li>
-              <a className="btn-sm text-gray-200 bg-gray-800 hover:bg-gray-900 shadow" href="signup.html">Register</a>
+              <Link
+                className="btn-sm text-gray-200 bg-gray-800 hover:bg-gray-900 shadow"
+                to="/signup"
+              >
+                Register
+              </Link>
             </li>
           </ul>
 
@@ -100,60 +156,59 @@ const SiteHeader = () => {
               __html: `
                <div class="flex md:hidden" x-data="{ expanded: false }">
 
-                        <!-- Hamburger button -->
-                        <button class="group inline-flex w-8 h-8 text-gray-800 bg-white text-center items-center justify-center transition" aria-controls="mobile-nav" :aria-expanded="expanded" @click.stop="expanded = !expanded">
-                            <span class="sr-only">Menu</span>
-                            <svg class="w-4 h-4 fill-current pointer-events-none" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
-                                <rect class="origin-center group-[[aria-expanded=true]]:rotate-[315deg] group-[[aria-expanded=true]]:[y:7] group-[[aria-expanded=true]]:[x:0] transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.1)]" y="2" x="7" width="9" height="2" rx="1"></rect>
-                                <rect class="origin-center group-[[aria-expanded=true]]:rotate-45 transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.8)]" y="7" width="16" height="2" rx="1"></rect>
-                                <rect class="origin-center group-[[aria-expanded=true]]:rotate-[135deg] group-[[aria-expanded=true]]:[y:7] group-[[aria-expanded=true]]:[x:0] transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.1)]" y="12" width="9" height="2" rx="1"></rect>
-                            </svg>
-                        </button>
+                <!-- Hamburger button -->
+                <button class="group inline-flex w-8 h-8 text-gray-800 bg-white text-center items-center justify-center transition" aria-controls="mobile-nav" :aria-expanded="expanded" @click.stop="expanded = !expanded">
+                    <span class="sr-only">Menu</span>
+                    <svg class="w-4 h-4 fill-current pointer-events-none" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+                        <rect class="origin-center group-[[aria-expanded=true]]:rotate-[315deg] group-[[aria-expanded=true]]:[y:7] group-[[aria-expanded=true]]:[x:0] transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.1)]" y="2" x="7" width="9" height="2" rx="1"></rect>
+                        <rect class="origin-center group-[[aria-expanded=true]]:rotate-45 transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.8)]" y="7" width="16" height="2" rx="1"></rect>
+                        <rect class="origin-center group-[[aria-expanded=true]]:rotate-[135deg] group-[[aria-expanded=true]]:[y:7] group-[[aria-expanded=true]]:[x:0] transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.1)]" y="12" width="9" height="2" rx="1"></rect>
+                    </svg>
+                </button>
 
-                        <!-- Mobile navigation -->
-                        <nav
-                            id="mobile-nav"
-                            class="absolute top-full z-20 left-0 w-full bg-white rounded-xl shadow-lg shadow-black/[0.03] before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(theme(colors.gray.100),theme(colors.gray.200))_border-box] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)] before:[mask-composite:exclude_!important] before:pointer-events-none"
-                            @click.outside="expanded = false"
-                            @keydown.escape.window="expanded = false"
-                            x-show="expanded"
-                            x-transition:enter="transition ease-out duration-200 transform"
-                            x-transition:enter-start="opacity-0 -translate-y-2"
-                            x-transition:enter-end="opacity-100 scale-100"
-                            x-transition:leave="transition ease-out duration-200"
-                            x-transition:leave-start="opacity-100"
-                            x-transition:leave-end="opacity-0"
-                            x-cloak
-                        >
-                            <ul class="text-sm p-2">
-                                <li>
-                                    <a class="flex text-gray-700 hover:bg-gray-100 rounded-lg py-1.5 px-2" href="pricing.html">Pricing</a>
-                                </li>
-                                <li>
-                                    <a class="flex text-gray-700 hover:bg-gray-100 rounded-lg py-1.5 px-2" href="customers.html">Customers</a>
-                                </li>
-                                <li>
-                                    <a class="flex text-gray-700 hover:bg-gray-100 rounded-lg py-1.5 px-2" href="blog.html">Blog</a>
-                                </li>
-                                <li>
-                                    <a class="flex text-gray-700 hover:bg-gray-100 rounded-lg py-1.5 px-2" href="documentation.html">Docs</a>
-                                </li>
-                                <li>
-                                    <a class="flex text-gray-700 hover:bg-gray-100 rounded-lg py-1.5 px-2" href="support.html">Support center</a>
-                                </li>
-                                <li>
-                                    <a class="flex text-gray-700 hover:bg-gray-100 rounded-lg py-1.5 px-2" href="apps.html">Apps</a>
-                                </li>
-                            </ul>
-                        </nav>
+                <!-- Mobile navigation -->
+                <nav
+                    id="mobile-nav"
+                    class="absolute top-full z-20 left-0 w-full bg-white rounded-xl shadow-lg shadow-black/[0.03] before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(theme(colors.gray.100),theme(colors.gray.200))_border-box] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)] before:[mask-composite:exclude_!important] before:pointer-events-none"
+                    @click.outside="expanded = false"
+                    @keydown.escape.window="expanded = false"
+                    x-show="expanded"
+                    x-transition:enter="transition ease-out duration-200 transform"
+                    x-transition:enter-start="opacity-0 -translate-y-2"
+                    x-transition:enter-end="opacity-100 scale-100"
+                    x-transition:leave="transition ease-out duration-200"
+                    x-transition:leave-start="opacity-100"
+                    x-transition:leave-end="opacity-0"
+                    x-cloak
+                >
+                    <ul class="text-sm p-2">
+                        <li>
+                            <a class="flex text-gray-700 hover:bg-gray-100 rounded-lg py-1.5 px-2 internal-link" href="/pricing">Pricing</a>
+                        </li>
+                        <li>
+                            <a class="flex text-gray-700 hover:bg-gray-100 rounded-lg py-1.5 px-2 internal-link" href="/customers">Customers</a>
+                        </li>
+                        <li>
+                            <a class="flex text-gray-700 hover:bg-gray-100 rounded-lg py-1.5 px-2 internal-link" href="/blog">Blog</a>
+                        </li>
+                        <li>
+                            <a class="flex text-gray-700 hover:bg-gray-100 rounded-lg py-1.5 px-2 internal-link" href="/documentation">Docs</a>
+                        </li>
+                        <li>
+                            <a class="flex text-gray-700 hover:bg-gray-100 rounded-lg py-1.5 px-2 internal-link" href="/support">Support center</a>
+                        </li>
+                        <li>
+                            <a class="flex text-gray-700 hover:bg-gray-100 rounded-lg py-1.5 px-2 internal-link" href="/apps">Apps</a>
+                        </li>
+                    </ul>
+                </nav>
 
-                    </div>`
+            </div>`,
             }}
-          >
-          </div>
+          ></div>
         </div>
-      </div >
-    </header >
+      </div>
+    </header>
   );
 };
 
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,23 +1,32 @@
-import { Navigate } from 'react-router-dom';
-import Layout from '../components/Layout';
-import Home from '../views/Home/index.jsx';
-
+import { Navigate } from "react-router-dom";
+import Layout from "../components/Layout";
+import Home from "../views/Home";
+import Pricing from "../views/Pricing";
+import Customers from "../views/Customers";
 
 const routes = [
   {
-    path: '/',
+    path: "/",
     element: <Layout />,
     children: [
       {
-        path: '/',
-        element: <Navigate to='/home' />,
+        path: "/",
+        element: <Navigate to="/home" />,
       },
       {
-        path: 'home',
+        path: "home",
         element: <Home />,
-      }
-    ]
-  }
+      },
+      {
+        path: "pricing",
+        element: <Pricing />,
+      },
+      {
+        path: "customers",
+        element: <Customers />,
+      },
+    ],
+  },
 ];
 
 export default routes;
